cf75226d896bcfa5ff6a8fe7064bda1e1a8df14f
MOONtyzoo <91515108+MOONtyzoo@users.noreply.github.com>
2024-07-05 09:16:26 -0700

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let outputArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            outputArray.push(array[i])
+        }
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
