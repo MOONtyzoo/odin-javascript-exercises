a5ceb0db2c08de04ba42ada5104b172a869c17e1
MOONtyzoo <91515108+MOONtyzoo@users.noreply.github.com>
2024-07-05 09:40:36 -0700

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (5.0/9.0)*(temp - 32)
+  return Math.round(temp*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (9.0/5.0)*temp + 32;
+  return Math.round(temp*10)/10
 };
 
 // Do not edit below this line
