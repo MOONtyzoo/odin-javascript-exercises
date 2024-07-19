7b5e62361b1222c38fe7b9408a5ddae74df65d57
MOONtyzoo <91515108+MOONtyzoo@users.noreply.github.com>
2024-07-19 12:24:04 -0700

    Complete exericise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replaceAll(/[^a-zA-Z0-9]/g, '')
+    console.log(str)
+    for (let i = 0; i < str.length/2; i++) {
+        if (str[i] != str[str.length-1-i]) return false
+    }
+    return true
 };
 
 // Do not edit below this line
