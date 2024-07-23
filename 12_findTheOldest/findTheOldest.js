d8a0390a295782467ae4eb67b1619669be31980d
MOONtyzoo <91515108+MOONtyzoo@users.noreply.github.com>
2024-07-22 19:33:41 -0700

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    let yearOfBirth = person.yearOfBirth
+    let yearOfDeath = person.yearOfDeath
+    if (yearOfDeath == undefined) {
+        let currentDate = new Date(Date.now())
+        yearOfDeath = currentDate.getFullYear()
+        console.log(yearOfDeath)
+    }
+    
+    let age = yearOfDeath - yearOfBirth
+    return age
+}
 
+const biggestNumber = function(acc, num) {
+    if (num > acc) { acc = num }
+    return acc;
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let oldestAge = getAge(oldest)
+        let thisAge = getAge(person)
+        if (thisAge > oldestAge) {oldest = person}
+        return oldest
+    })
 };
 
 // Do not edit below this line
