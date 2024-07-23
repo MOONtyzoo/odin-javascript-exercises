428a30ecc1dfd4a0ff1f6e79042d06b532b60d55
MOONtyzoo <91515108+MOONtyzoo@users.noreply.github.com>
2024-07-22 18:48:46 -0700

    Complete exercise 10_Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let index = num
+    if (!Number.isInteger(num))
+        index = parseInt(num)
+    if (index < 0) {return "OOPS"}
 
+    let fibonacciArray = [0, 1, 1]
+    for (let i = fibonacciArray.length; i <= index; i++) {
+        fibonacciArray.push(fibonacciArray[i-1]+fibonacciArray[i-2])
+    }
+    return fibonacciArray[index]
 };
 
 // Do not edit below this line
